8660fe8ccf798f8f74f5617d42e8ac4faf588bc4
Alex-La <lars6162@gmail.com>
2021-03-08 21:38:33 +0300

    Set my tickets preset

--- client/src/Components/Game/Body/Tickets.jsx
+++ client/src/Components/Game/Body/Tickets.jsx
@@ -1,3 +1,5 @@
+import Ticket from "../../../img/ticket.png";
+
 const Tickets = () => {
   const buyTicket = () => console.log("Buy ticket");
 
@@ -6,6 +8,15 @@ const Tickets = () => {
       <a onClick={buyTicket}>
         <div className="btn">BUY</div>
       </a>
+
+      <div className="ticket-title">
+        <img src={Ticket} alt="ticket" />
+        <p className="p7">
+          My tickets <span style={{ color: "#3897f1" }}>{" " + 4}</span>
+        </p>
+      </div>
+
+      <div className="tickets_"></div>
     </div>
   );
 };
