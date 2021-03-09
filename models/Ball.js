b226292a9373b826371b44d981aa50f3059336f4
Alex-La <lars6162@gmail.com>
2021-03-09 05:14:43 +0300

    get winners api

---
+++ models/Ball.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+
+const schema = new Schema({
+  name: { type: String, required: true, unique: false },
+  value: { type: Array, required: true },
+  time: { type: String, required: false },
+  players: { type: Number, required: false },
+});
+
+module.exports = model("Shary", schema);
--- routes/tron.js
+++ routes/tron.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const TronWeb = require("tronweb");
+const TronGrid = require("trongrid");
 const User = require("../models/User");
+const Ball = require("../models/Ball");
 const { paginateResults, getAvatarPath } = require("../utils");
 
 const TRONGRID_API = "https://api.shasta.trongrid.io";
@@ -10,6 +12,18 @@ const tronweb = new TronWeb(TRONGRID_API, TRONGRID_API, TRONGRID_API);
 tronweb.defaultAddress = {
   hex: tronweb.address.toHex(DEFAULT_ADDRESS),
   base58: DEFAULT_ADDRESS,
+};
+
+const trongrid = new TronGrid(tronweb);
+
+const gamesAddress = {
+  LimitLottery5: "TRGCoM8ForcJhHXCE3RsWouF14V3rPixSu",
+  LimitLottery15: "TArRpQXzAutbdn3rZfPmMQzHx2rn4uRGy5",
+  LimitLottery50: "TSmijEjGX7F2MY9nUJbaiB1xqgVbN7bYvX",
+  Everyweek5: "TYk1bmKpaASD8MHLXr6QipNYdtUAAHno4f",
+  Everyweek50: "TKF9zmQpKHPgA9FxuSVHpogiucTGVRHBWN",
+  Month5: "TVGniJKSx13v74zfwZL16pjvGUVf4xQynD",
+  EveryYear5: "TBcYVCEM5Y2dXVGg7ojTyN6pHeuWnRThBf",
 };
 
 const games = {
@@ -33,10 +47,145 @@ const addresses = {
   }
 })();
 
+(async () => {
+  for (const [key, value] of Object.entries(addresses)) {
+    addresses[key] = await tronweb.contract().at(value);
+  }
+})();
+
 //-------------Timer-----------------------------------
 let weekTime = 0;
 let monthTime = 0;
 let yearTime = 0;
+//-----------------------------------------------------
+
+router.get("/allgames", async (_, res) => {
+  try {
+    const data = [];
+    for (const [_, value] of Object.entries(games)) {
+      const obj = {
+        sum: Math.floor(
+          tronweb.toDecimal((await value.getSumOnContract().call())._hex) / 1e12
+        ),
+        ticketsLength: tronweb.toDecimal(
+          (await value.getTicketsLength().call())._hex
+        ),
+      };
+      data.push(obj);
+    }
+    res.json([...data, { weekTime, monthTime, yearTime }]);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Server error!" });
+  }
+});
+
+router.get("/owners/:contract", async (req, res) => {
+  try {
+    const data = await (await games[req.params.contract])
+      .ownersOfTickets()
+      .call();
+    const wallets = data.map((wallet) => tronweb.address.fromHex(wallet));
+    const users = await User.find({ wallet: wallets });
+
+    const result = wallets.map((wallet, index) => {
+      const temp = users.filter((user) => wallet === user.wallet)[0];
+      if (temp)
+        return {
+          name: temp.name,
+          cursorKey: index,
+          avatar: getAvatarPath({ id: temp._id }),
+        };
+      else
+        return {
+          name: wallet.substr(0, 6) + "..." + wallet.substr(30, 4),
+          cursorKey: index,
+          avatar: getAvatarPath({ id: 0 }),
+        };
+    });
+
+    const pagResult = paginateResults({
+      results: result,
+      pageSize: 10,
+      after: req.query.after,
+      cursorKey: "cursorKey",
+    });
+
+    res.json({
+      total: wallets.length,
+      cursor: pagResult.length
+        ? pagResult[pagResult.length - 1].cursorKey
+        : null,
+      hasMore: pagResult.length
+        ? pagResult[pagResult.length - 1].cursorKey !==
+          result[result.length - 1].cursorKey
+        : false,
+      owners: pagResult,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Server error!" });
+  }
+});
+
+router.get("/balance/:contract", async (req, res) => {
+  try {
+    const data = await (await games[req.params.contract])
+      .getSumOnContract()
+      .call();
+
+    const balance = tronweb.toDecimal(data._hex);
+    res.json({ balance: Math.floor(balance / 1e12) });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Server error!" });
+  }
+});
+
+router.get("/winners/:contract", async (req, res) => {
+  try {
+    const { contract } = req.params;
+    const names = ["firstWinner", "secondWinner"];
+
+    const trxPrice = tronweb.toDecimal(
+      (await addresses.SevenTOP.trxPrice().call())._hex
+    );
+    const data = [];
+
+    for (let i in names) {
+      const trans = await trongrid.contract.getEvents(gamesAddress[contract], {
+        event_name: names[i],
+      });
+      if (trans.data[0])
+        data.push({
+          amount: ((trans.data[0].result.amount * trxPrice) / 1e12).toFixed(2),
+          timestapmt: trans.data[0].result.timestapmt,
+          user: tronweb.address.fromHex(trans.data[0].result.user),
+        });
+    }
+
+    const result = [];
+    for (let i in data) {
+      result.push({
+        ...data[i],
+        user: (({ _id, name }) => ({
+          _id,
+          name,
+          avatar: getAvatarPath({ id: _id }),
+        }))(await User.findOne({ wallet: data[i].user })),
+      });
+    }
+
+    res.json(result);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Server error!" });
+  }
+});
+
+module.exports = router;
+
+//--------------------------------------------------------------------------
 
 function diffSubtract(date1, date2) {
   return date2 - date1;
@@ -122,89 +271,3 @@ const yeartimer = setInterval(
   () => funcaYear(new Date(end_date_str), yeartimer),
   1000
 );
-//-----------------------------------------------------------
-
-router.get("/allgames", async (_, res) => {
-  try {
-    const data = [];
-    for (const [_, value] of Object.entries(games)) {
-      const obj = {
-        sum: Math.floor(
-          tronweb.toDecimal((await value.getSumOnContract().call())._hex) / 1e12
-        ),
-        ticketsLength: tronweb.toDecimal(
-          (await value.getTicketsLength().call())._hex
-        ),
-      };
-      data.push(obj);
-    }
-    res.json([...data, { weekTime, monthTime, yearTime }]);
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ message: "Server error!" });
-  }
-});
-
-router.get("/owners/:contract", async (req, res) => {
-  try {
-    const data = await (await games[req.params.contract])
-      .ownersOfTickets()
-      .call();
-    const wallets = data.map((wallet) => tronweb.address.fromHex(wallet));
-    const users = await User.find({ wallet: wallets });
-
-    const result = wallets.map((wallet, index) => {
-      const temp = users.filter((user) => wallet === user.wallet)[0];
-      if (temp)
-        return {
-          name: temp.name,
-          cursorKey: index,
-          avatar: getAvatarPath({ id: temp._id }),
-        };
-      else
-        return {
-          name: wallet.substr(0, 6) + "..." + wallet.substr(30, 4),
-          cursorKey: index,
-          avatar: getAvatarPath({ id: 0 }),
-        };
-    });
-
-    const pagResult = paginateResults({
-      results: result,
-      pageSize: 10,
-      after: req.query.after,
-      cursorKey: "cursorKey",
-    });
-
-    res.json({
-      total: wallets.length,
-      cursor: pagResult.length
-        ? pagResult[pagResult.length - 1].cursorKey
-        : null,
-      hasMore: pagResult.length
-        ? pagResult[pagResult.length - 1].cursorKey !==
-          result[result.length - 1].cursorKey
-        : false,
-      owners: pagResult,
-    });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ message: "Server error!" });
-  }
-});
-
-router.get("/balance/:contract", async (req, res) => {
-  try {
-    const data = await (await games[req.params.contract])
-      .getSumOnContract()
-      .call();
-
-    const balance = tronweb.toDecimal(data._hex);
-    res.json({ balance: Math.floor(balance / 1e12) });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ message: "Server error!" });
-  }
-});
-
-module.exports = router;
