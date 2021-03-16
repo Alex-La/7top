c12a5b432f9068bdbfd5cf609521ae4fd7bd90c1
Alex-La <lars6162@gmail.com>
2021-03-17 00:57:29 +0300

    Update tickets loading

--- client/src/Components/Game/Body/Tickets.jsx
+++ client/src/Components/Game/Body/Tickets.jsx
@@ -5,70 +5,55 @@ import { useSelector, useDispatch } from "react-redux";
 import { getBalance, buyAction } from "../../../redux/actions/tronActions";
 
 import useTronWeb from "../../../hooks/tronweb.hook";
-import useMessage from "../../../hooks/message.hook";
 
 import Ticketimg from "../../../img/Ticketimg.png";
 
-const games = {
-  LimitLottery5: "TRGCoM8ForcJhHXCE3RsWouF14V3rPixSu",
-  LimitLottery15: "TArRpQXzAutbdn3rZfPmMQzHx2rn4uRGy5",
-  LimitLottery50: "TSmijEjGX7F2MY9nUJbaiB1xqgVbN7bYvX",
-  Everyweek5: "TYk1bmKpaASD8MHLXr6QipNYdtUAAHno4f",
-  Everyweek50: "TKF9zmQpKHPgA9FxuSVHpogiucTGVRHBWN",
-  Month5: "TVGniJKSx13v74zfwZL16pjvGUVf4xQynD",
-  EveryYear5: "TBcYVCEM5Y2dXVGg7ojTyN6pHeuWnRThBf",
-};
-
 const Tickets = () => {
-  const message = useMessage();
-  const { buyTicket, error, clearError } = useTronWeb();
+  const { buyTicket, myTickets } = useTronWeb();
   const dispatch = useDispatch();
-  const [myTickets, setMyTickets] = useState([]);
   const { contract, me, owners } = useSelector(({ contract, me, owners }) => ({
     contract,
     me,
     owners,
   }));
 
-  const getMyTickets = useCallback(async () => {
-    if (window.tronWeb && window.tronWeb.ready && me) {
-      const lottery = await window.tronWeb.contract().at(games[contract]);
-      const ticks = await lottery.getMyTickets(me.wallet).call();
-      const arrayOfTicks = [];
-      for (let i in ticks) {
-        arrayOfTicks.push(window.tronWeb.toDecimal(ticks[i]._hex));
-      }
-      setMyTickets(arrayOfTicks);
-    }
-  }, [me, contract]);
+  // const getMyTickets = useCallback(async () => {
+  //   if (window.tronWeb && window.tronWeb.ready && me) {
+  //     const lottery = await window.tronWeb.contract().at(games[contract]);
+  //     const ticks = await lottery.getMyTickets(me.wallet).call();
+  //     const arrayOfTicks = [];
+  //     for (let i in ticks) {
+  //       arrayOfTicks.push(window.tronWeb.toDecimal(ticks[i]._hex));
+  //     }
+  //     setMyTickets(arrayOfTicks);
+  //   }
+  // }, [me, contract]);
 
-  useEffect(() => {
-    let tries = 0;
-    const inter = setInterval(() => {
-      if (window.tronWeb) {
-        clearInterval(inter);
-        getMyTickets();
-      } else tries++;
+  // useEffect(() => {
+  //   let tries = 0;
+  //   const inter = setInterval(() => {
+  //     if (window.tronWeb) {
+  //       clearInterval(inter);
+  //       getMyTickets();
+  //     } else tries++;
 
-      if (tries === 10) clearInterval(inter);
-    }, 1000);
-  }, [getMyTickets]);
+  //     if (tries === 10) clearInterval(inter);
+  //   }, 1000);
+  // }, [getMyTickets]);
 
-  useEffect(() => {
-    if (error === "Success!") {
-      dispatch(buyAction({ me }));
-      dispatch(getBalance({ contract }));
-      setMyTickets([owners.total, ...myTickets]);
-    }
-    message(error);
-    clearError();
-  }, [error, clearError, message, dispatch, getMyTickets, me, contract]);
-
-  const buy = () => buyTicket(games[contract]);
+  // useEffect(() => {
+  //   if (error === "Success!") {
+  //     dispatch(buyAction({ me }));
+  //     dispatch(getBalance({ contract }));
+  //     setMyTickets([owners.total, ...myTickets]);
+  //   }
+  //   message(error);
+  //   clearError();
+  // }, [error, clearError, message, dispatch, getMyTickets, me, contract]);
 
   return (
     <Fragment>
-      <a onClick={buy}>
+      <a onClick={buyTicket}>
         <div className="btn">BUY</div>
       </a>
 
--- client/src/hooks/tronweb.hook.js
+++ client/src/hooks/tronweb.hook.js
@@ -1,50 +1,78 @@
-import { useCallback, useState } from "react";
-import { useSelector } from "react-redux";
+import { useCallback, useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { buyAction, getBalance } from "../redux/actions/tronActions";
+
+import useMessage from "./message.hook";
 
 const useTronWeb = () => {
-  const [error, setError] = useState(null);
-  const me = useSelector(({ me }) => me);
+  const [myTickets, setMyTickets] = useState([]);
 
-  const checkError = useCallback(() => {
-    try {
-      if (!window.tronWeb) throw new Error("Please, install TronLink!");
-      if (!window.tronWeb.ready) throw new Error("Please, login TronLink");
-      if (!me) throw new Error("Please, authorize");
-      if (process.env.NODE_ENV === "production") {
-        if (window.tronWeb.fullNode.host !== "https://api.shasta.trongrid.io")
-          throw new Error("Please, change node to TronGrid");
-      } else {
-        if (window.tronWeb.fullNode.host !== "https://api.shasta.trongrid.io")
-          throw new Error("Please, change node to Shasta TronGrid");
-      }
-      return false;
-    } catch (e) {
-      setError(e.message);
-      return true;
-    }
-  }, [me]);
-
-  const buyTicket = useCallback(
-    async (address) => {
-      try {
-        if (checkError()) return;
-        const lottery = await window.tronWeb.contract().at(address);
-        await lottery.buyTicket().send({
-          feeLimit: 30_000_000,
-          callValue: await lottery.calculateTrx(1).call(),
-        });
-        setError("Success!");
-      } catch (e) {
-        console.log(e);
-        setError(e);
-      }
-    },
-    [checkError]
+  const message = useMessage();
+  const dispatch = useDispatch();
+  const { tronWeb, me, contract, owners } = useSelector(
+    ({ tronWeb, me, contract, owners }) => ({
+      tronWeb,
+      me,
+      contract,
+      owners,
+    })
   );
 
-  const clearError = useCallback(() => setError(null), []);
+  const checkAuth = useCallback(() => {
+    if (me) return false;
+    else message("Please, authorize");
+    return true;
+  }, [me]);
 
-  return { buyTicket, error, clearError };
+  const checkTronWeb = useCallback(() => {
+    if (tronWeb.instance === null) {
+      message("Please, install tronLink");
+      return true;
+    }
+    if (tronWeb.instance === false) {
+      message("Please, login tronLink");
+      return true;
+    }
+    if (tronWeb.instance.fullNode.host !== "https://api.shasta.trongrid.io") {
+      message("Please, change node to Shasta TronGrid");
+      return true;
+    }
+    return false;
+  }, [tronWeb, message]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (tronWeb.instance && !me.wallet) return;
+        const lottery = await tronWeb.instance.contract().at(tronWeb[contract]);
+        const ticks = await lottery.getMyTickets(me.wallet).call();
+        const arrayOfTicks = [];
+        for (let i in ticks) {
+          arrayOfTicks.push(tronWeb.instance.toDecimal(ticks[i]._hex));
+        }
+        setMyTickets(arrayOfTicks);
+      } catch (e) {}
+    })();
+  }, [checkTronWeb, me, contract]);
+
+  const buyTicket = useCallback(async () => {
+    try {
+      if (checkTronWeb() || checkAuth()) return;
+      const lottery = await tronWeb.instance.contract().at(tronWeb[contract]);
+      await lottery.buyTicket().send({
+        feeLimit: 30_000_000,
+        callValue: await lottery.calculateTrx(1).call(),
+      });
+      message("Success!");
+      dispatch(buyAction({ me }));
+      dispatch(getBalance({ contract }));
+      setMyTickets([owners.total, ...myTickets]);
+    } catch (e) {
+      message(e);
+    }
+  }, [checkTronWeb, dispatch, me, contract, message]);
+
+  return { buyTicket, myTickets };
 };
 
 export default useTronWeb;
